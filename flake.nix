5b6806aedb82dd8591e0764a468482801b9cb954
Ian Kilgore <iank@iank.org>
2025-12-29 16:24:07 -0500

    Separate desktop configuration

--- flake.nix
+++ flake.nix
@@ -11,20 +11,21 @@
 
   outputs = { self, nixpkgs, home-manager, ... }:
   let
-    mkHome = system: username:
+    mkHome = system: username: module:
       let
         pkgs = import nixpkgs { inherit system; };
         homeDir = "/home/${username}";
       in
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [ ./home.nix ];
+          modules = [ module ];
           extraSpecialArgs = { inherit username homeDir; };
         };
   in {
     homeConfigurations = {
-      ian = mkHome "x86_64-linux" "ian";
-      ian-arm = mkHome "aarch64-linux" "ian";
+      ian = mkHome "x86_64-linux" "ian" ./home.nix;
+      ian-arm = mkHome "aarch64-linux" "ian" ./home.nix;
+      ian-desktop = mkHome "x86_64-linux" "ian" ./desktop.nix;
     };
   };
 }
